51984b8dcc53223d307b5a48ce9e1a2b8fd3ae63
rahulshukla <shuklarahul000051@gmail.com>
2023-12-22 02:10:43 +0530

    feat<wip>: design table ui

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -4,9 +4,9 @@ import "../styles/table.css";
 export default function Table() {
     return (
         <>
-            <div style={{ border: "2px solid black", padding: 5, marginBottom: "5px" }}>3rd div</div>
-
-            <Weekday />
+            <div style={{ border: "2px solid black", padding: 5, marginBottom: "5px" }}>
+                <Weekday />
+            </div>            
         </>
     )
 }
